370c8423f43254abd7488c89157ff3519f025ad5
Jameson Hill <stevenjamesonhill@gmail.com>
2017-03-31 14:07:18 -0400

    recipes add

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const {ShoppingList} = require('./models');
+const {Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -22,6 +23,15 @@ ShoppingList.create('peppers', 4);
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+
+
+Recipes.create('spaghetti', ['pasta', 'tomato sauce', 'meatballs']);
+Recipes.create('cereal', ['milk', 'cereal']);
+Recipes.create('breakfast sandwich', ['bagel', 'eggs', 'bacon', 'cheese']);
+
+app.get('/Recipes', (req, res) => {
+  res.json(createRecipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
